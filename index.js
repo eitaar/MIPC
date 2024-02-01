25fb9c5d5ad4cf8bad57e93aa52f7c06bbb737b3
eitraaa <154091730+eitraaa@users.noreply.github.com>
2024-02-01 18:50:03 +0000

    upd

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const http = require('http');
-const {bestprice} = require('./db.js');
-const { helpembed, itemlistembed } = require('./embeds.js');
-const {token} = process.env
-const { Client, GatewayIntentBits} = require('discord.js');
+const { bestprice } = require('./db.js');
+const { token } = process.env;
+const { Client, GatewayIntentBits } = require('discord.js');
 const deploy_commands = require("./other/deploy-commands.js");
 const interaction_create_handler = require("./other/interaction_create_handler.js");
 const load_commands = require("./other/load_commands.js");
+
 const bot = new Client({
     intents: [
         GatewayIntentBits.Guilds,
@@ -18,7 +18,7 @@ const bot = new Client({
 interaction_create_handler.call(bot);
 deploy_commands.start();
 
-http.createServer(function(request,response) {
+http.createServer(function (request, response) {
     response.writeHead(200, { 'Content-Type': 'text/plain' });
     response.end('Bot is online!');
 }).listen(3000);
@@ -29,19 +29,15 @@ bot.on('ready', async () => {
     bot.channels.cache.get('1174045804906938368').send('再起動');
 });
 
-
 bot.on('messageCreate', async (msg) => {
-  if (msg.author.bot) return;
- if (msg.content.startsWith('!test')) {
-   const item = msg.content.split(' ')[1];
-   const get = await bestprice(item, 1, 0);
-   console.log(get);
-   const a = get[1][2]
-   console.log(a);
-   msg.channel.send(`mre: ${a}`);
-   }
+    if (msg.author.bot) return;
+    if (msg.content.startsWith('!test')) {
+        const item = msg.content.split(' ')[1];
+        const get = await bestprice(item, 1, 0);
+        const a = get[1][2];
+        console.log(a);
+        msg.channel.send(`mre: ${a}`);
+    }
 });
-
-
 
 bot.login(token);
